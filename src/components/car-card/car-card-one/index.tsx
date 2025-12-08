b30e7c7bf194838babba08f02a2421fa3c127e02
hassam55-dev <hassam@asintelligence.co.uk>
2025-12-08 15:05:34 +0500

    reversed gap and paddings only

--- src/components/car-card/car-card-one/index.tsx
+++ src/components/car-card/car-card-one/index.tsx
@@ -100,6 +100,23 @@ function CarCard({ car, styles }: { car: Car; styles: any }) {
       <div
         className={`${styles["car-card__actions"]} opacity-0 group-hover:opacity-100`}
       >
+        <Button
+          variant="secondary"
+          btnText="View Car"
+          clickEvent={() => {
+            navigate(`/car-details?stockId=${stockId}`);
+          }}
+        />
+
+        <Button
+          variant={car.isReserved ? "disabled" : "secondary"}
+          btnText="Reserve for £99"
+          clickEvent={() => {
+            setReservationModalOpen(true);
+            setReservationCarData(car);
+          }}
+        />
+
         {dealerData.FCANumber && (
           <Button
             variant={car.isReserved ? "disabled" : "secondary"}
@@ -112,22 +129,6 @@ function CarCard({ car, styles }: { car: Car; styles: any }) {
           />
         )}
 
-        <Button
-          variant={car.isReserved ? "disabled" : "secondary"}
-          btnText="Reserve for £99"
-          clickEvent={() => {
-            setReservationModalOpen(true);
-            setReservationCarData(car);
-          }}
-        />
-
-        <Button
-          variant="secondary"
-          btnText="View Car"
-          clickEvent={() => {
-            navigate(`/car-details?stockId=${stockId}`);
-          }}
-        />
         {/* <div
           className={`${styles["view-car__buttons-wrapper"]} flex justify-between items-center`}
         >
@@ -151,12 +152,12 @@ function CarCard({ car, styles }: { car: Car; styles: any }) {
         </div> */}
       </div>
 
-      <div className="flex flex-col gap-3 px-4 pb-3 md:hidden">
+      <div className="flex flex-col gap-1 px-4 md:hidden">
         <Button
           variant="secondary"
           btnText="View Car"
           btnTextSize="text-xs"
-          paddingUtilities="px-4 py-3"
+          paddingUtilities="px-3 py-2"
           clickEvent={() => {
             navigate(`/car-details?stockId=${stockId}`);
           }}
@@ -166,7 +167,7 @@ function CarCard({ car, styles }: { car: Car; styles: any }) {
           variant={car.isReserved ? "disabled-mobile" : "secondary"}
           btnText="Reserve for £99"
           btnTextSize="text-xs"
-          paddingUtilities="px-4 py-3"
+          paddingUtilities="px-3 py-2"
           clickEvent={() => {
             setReservationModalOpen(true);
             setReservationCarData(car);
@@ -178,7 +179,7 @@ function CarCard({ car, styles }: { car: Car; styles: any }) {
             variant={car.isReserved ? "disabled-mobile" : "secondary"}
             btnText="Apply Finance"
             btnTextSize="text-xs"
-            paddingUtilities="px-4 py-3"
+            paddingUtilities="px-3 py-2"
             clickEvent={() => {
               navigate(
                 `/car-details?stockId=${stockId}#codeweaver-finance-section`
